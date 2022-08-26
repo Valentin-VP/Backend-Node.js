687cf974a16b64e7fba9bdc5ba893e0974a8523b
vvasconcellos <vvasconcellos@baufest.com>
2022-08-26 08:53:13 -0300

    users, categories, orders logic

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -1,22 +1,73 @@
 const express = require('express');
 
+const CategoryService = require('./../services/category.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { createCategorySchema, updateCategorySchema, getCategorySchema } = require('./../schemas/category.schema');
+
 const router = express.Router();
-// /api/v1/categories
+const service = new CategoryService();
 
-// middleware específico a este router sirve para tener un control de cuando se accede a un endpoint
-router.use('/', function (req, res, next) {
-  console.log('Accediendo a categories');
-  next(); // se utiliza para que se ejecute el router.get
+router.get('/', async (req, res, next) => {
+  try {
+    const categories = await service.find();
+    res.json(categories);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/categories/:catId/products/:prodId', (req, res) => {
-  const { catId, prodId } = req.params;
-  res.json({
-    product: {
-      prodId,
-      catId,
-    },
-  });
-});
+router.get('/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const category = await service.findOne(id);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post('/',
+  validatorHandler(createCategorySchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newCategory = await service.create(body);
+      res.status(201).json(newCategory);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.patch('/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  validatorHandler(updateCategorySchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const category = await service.update(id, body);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete('/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(201).json({id});
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
