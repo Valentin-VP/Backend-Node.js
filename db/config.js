c2ba430637758148b83abedbe7c98360889e4085
vvasconcellos <vvasconcellos@baufest.com>
2022-08-29 10:57:24 -0300

    migrations db working

--- db/config.js
+++ db/config.js
@@ -1,17 +1,16 @@
-const { Sequelize } = require('sequelize');
-
 const { config } = require('../config/config');
-const  { setUpModels } = require('../db/models');
 
 const USER = encodeURIComponent(config.db.user);
 const PASSWORD = encodeURIComponent(config.db.password);
 const URI = `postgres://${USER}:${PASSWORD}@${config.db.host}:${config.db.port}/${config.db.database}`;
 
-const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
-  logging: true,
-});
-
-setUpModels(sequelize);
-
-module.exports = sequelize;
+module.exports = {
+  development: {
+    url: URI,
+    dialect: 'postgres',
+  },
+  production: {
+    url: URI,
+    dialect: 'postgres',
+  },
+};
